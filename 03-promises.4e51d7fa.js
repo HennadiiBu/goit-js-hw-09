03a7ef82d6f7538fe259c129953eee3737354159
HennadiiBu <99074724+HennadiiBu@users.noreply.github.com>
2023-04-16 21:36:52 +0000

    Deploying to gh-pages from @ HennadiiBu/goit-js-hw-09@2f17926cddd4f5eb5f82687ccd99d168398ba358 🚀

--- 03-promises.4e51d7fa.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("button");document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');e.addEventListener("submit",(function(e){e.preventDefault(),console.log("123")}));
-//# sourceMappingURL=03-promises.4e51d7fa.js.map
--- 03-promises.dbe5c9aa.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("button");document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');e.addEventListener("submit",(function(e){e.preventDefault(),console.log("123")}))}();
-//# sourceMappingURL=03-promises.dbe5c9aa.js.map
