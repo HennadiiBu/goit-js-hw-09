94c9f2d2d496f0659adb478c7be79334e1a70017
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-04-16 23:08:15 +0300

    task-3 start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,30 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+}
+
+const createPromicesBtn = document.querySelector('button');
+const promiceDelay = document.querySelector('input[name="delay"]');
+const promiceStep = document.querySelector('input[name="step"]');
+const promiceAmount = document.querySelector('input[name="amount"]');
+
+createPromicesBtn.addEventListener('submit', onCreatePromices);
+
+function onCreatePromices(event) {
+  event.preventDefault();
+
+  let amount = promiceAmount.value;
+  console.log(promiceDelay.value);
+  const promice = setInterval(() => {
+    if (amount < 0) {
+      clearInterval(promice);
+    }
+    createPromise(amount, promiceDelay.value);
+    amount -= 1;
+  }, promiceStep);
 }
