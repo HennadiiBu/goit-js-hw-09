2f17926cddd4f5eb5f82687ccd99d168398ba358
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-04-17 00:36:28 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,21 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // Reject
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+
+  const promise = new Promise(() => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      } else {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      }
+    }, delay);
+  });
 }
 
 const createPromicesBtn = document.querySelector('button');
@@ -14,19 +23,21 @@ const promiceDelay = document.querySelector('input[name="delay"]');
 const promiceStep = document.querySelector('input[name="step"]');
 const promiceAmount = document.querySelector('input[name="amount"]');
 
-createPromicesBtn.addEventListener('submit', onCreatePromices);
+createPromicesBtn.addEventListener('click', onCreatePromices);
 
 function onCreatePromices(event) {
   event.preventDefault();
+  let counter = 1;
+  let amount = Number(promiceAmount.value);
+  let startDelay = Number(promiceDelay.value);
 
-  console.log(`123`);
-  // let amount = promiceAmount.value;
-
-  // const promice = setInterval(() => {
-  //   if (amount < 0) {
-  //     clearInterval(promice);
-  //   }
-  //   createPromise(amount, promiceDelay.value);
-  //   amount -= 1;
-  // }, promiceStep);
+  const promice = setInterval(() => {
+    if (counter === amount) {
+      clearInterval(promice);
+    }
+    createPromise(counter, startDelay);
+    counter += 1;
+    startDelay =
+      Number(promiceDelay.value) + Number(promiceStep.value) * (counter - 1);
+  }, Number(promiceStep.value) * (counter - 1));
 }
